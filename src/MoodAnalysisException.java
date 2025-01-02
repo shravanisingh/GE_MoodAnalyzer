635d6b9cb5a6ccf928b59780ec1fbe89cdc93faa
shravnisingh <shravanisingh22@gmail.com>
2025-01-03 00:38:46 +0530

    UC-03

---
+++ src/MoodAnalysisException.java
@@ -0,0 +1,10 @@
+public class MoodAnalysisException extends Exception {
+    private final MoodAnalysisError error;
+
+    public MoodAnalysisException(MoodAnalysisError error, String message){
+        super(message);
+        this.error = error;
+    }
+
+
+}
