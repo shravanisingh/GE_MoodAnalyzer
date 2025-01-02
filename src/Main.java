bf358f0c6d0eb3cdd73e32bddf6e1869ffea7e3f
shravnisingh <shravanisingh22@gmail.com>
2025-01-03 00:34:49 +0530

    UC-02

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        // Test Case 1: Constructor with "I am in Sad Mood"
         MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in Sad Mood");
         String result1 = moodAnalyser1.analyseMood();
         System.out.println("Message: " + moodAnalyser1.getMessage() + " | Mood: " + result1);
 
-        MoodAnalyser moodAnalyser2 = new MoodAnalyser();
-        moodAnalyser2.setMessage("I am in Any Mood");
+        // Test Case 1.1: Constructor with "I am in Happy Mood"
+        MoodAnalyser moodAnalyser2 = new MoodAnalyser("I am in Happy Mood");
         String result2 = moodAnalyser2.analyseMood();
         System.out.println("Message: " + moodAnalyser2.getMessage() + " | Mood: " + result2);
+
+        // Test Case 2: Constructor with null mood
+        MoodAnalyser moodAnalyser3 = new MoodAnalyser(null);
+        String result3 = moodAnalyser3.analyseMood();
+        System.out.println("Message: " + moodAnalyser3.getMessage() + " | Mood: " + result3);
     }
 }
